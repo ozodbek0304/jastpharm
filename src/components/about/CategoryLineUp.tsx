f219d6994c0ffd0fa54a488451668ca32ba246cd
doniyor <edoniyorbekdev@gmail.com>
2024-05-21 11:49:44 +0500

    popolary products

--- src/components/about/CategoryLineUp.tsx
+++ src/components/about/CategoryLineUp.tsx
@@ -5,12 +5,15 @@ import useResponsive from "@/hooks/use-responsive";
 import { brandData } from "./PartnersMain";
 import api from "@/utils/api";
 import parse from 'html-react-parser'
+import { useTranslation } from "react-i18next";
 
 const CategoryLineUp = () => {
 
   const { isMobile } = useResponsive()
   const [timer, setTimer] = useState(0)
-  const [data, setData] = useState<any>(null)
+  const [data, setData] = useState<any>([])
+
+  const { t } = useTranslation()
 
   const showItems = () => {
     const interval = setInterval(() => {
@@ -36,44 +39,41 @@ const CategoryLineUp = () => {
       <div className="category-lineup-area">
         <div className="container">
           <div className="row pt-20 gy-5">
-            <div className={`col-md-5 fade ${timer > 0 ? "show" : ""}`}>
-              <Image src={data?.image} width={1200} height={isMobile ? 200 : 300} alt="product" style={{ objectFit: 'cover' }} />
-            </div>
-            <div className={`col-md-7 fade ${timer > 1 ? "show" : ""}`}>
-              <div className="category-wrapper category-wrapper1">
-                {data?.body && parse(data?.body)}
-              </div>
-            </div>
-            <div className={`col-md-7 fade ${timer > 2 ? "show" : ""}`}>
-              <div className="category-wrapper category-wrapper1">
-                Наша история началась в 2011 году, когда возникали большие трудности в ведении бизнеса с иностранными партнерами. Несмотря на все преграды, компания уверенными шагами внедряла новые технологии и обучала врачей Узбекистана. Оглядываясь назад, и, смотря на сегодняшние достижения врачей, мы с гордостью говорим, что мы не зря старались и верили в их результат.
-                Сегодня врачи в области кардиохирургии делают операции по установки стентов, и людям не приходиться выезжать в соседние страны для получения медицинского обслуживания. В Узбекистане растет и развивается сеть лабораторий VITROS, где функционирует наше оборудование от Johnson & Johnson, и мы рады, что население Узбекистана могут пройти диагностику на мировом оборудовании и получить точный и достоверный результат.
-                Еще одним нашим достижением является внедрение и бесперебойные поставки шовного хирургического материала ETHICON, мы гордимся, что наши хирурги, как хирурги других стран, пользуются и работают самыми популярными и качественными нитями. К 2022 году мы выросли по количеству компаний, входящих в состав группы компании MEDOL. На сегодняшний день успешно функционирует 4 организации, имеющих свою миссию, цель и сферу деятельности.
-              </div>
-            </div>
-            <div className={`col-md-5 fade ${timer > 3 ? "show" : ""}`}>
-              <Image src={"https://medol.uz//data/uploads/module/pages/144/original/63baf330b6056.jpg"} width={1200} height={isMobile ? 200 : 300} alt="product" style={{ objectFit: 'cover' }} />
-            </div>
-            <div className={`col-md-12 fade ${timer > 4 ? "show" : ""}`}>
-              <div className="category-wrapper category-wrapper1 mt-15">
-                <div className="category-wrapper category-wrapper1 mt-15 row gy-4 p-0 mx-auto">
-                  {data?.images.map((item: { image: string }, i: number) => (
-                    <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12" key={i}>
-                      <div className="text-center brand-img-div" style={{ border: '1px solid #c3cccc' }}>
-                        <Image
-                          className="d-inline-block brand-img"
-                          src={item.image}
-                          alt="brand-img"
-                          width={200}
-                          height={300}
-                          style={{ width: 'auto', height: 'auto' }}
-                        />
+            {
+              data.map((item: any, i: number) => (
+                <>
+                  <div key={i} className={`col-md-5 fade ${timer > 0 ? "show" : ""}`}>
+                    <Image src={item?.image} width={1200} height={isMobile ? 200 : 300} alt="product" style={{ objectFit: 'cover' }} />
+                  </div>
+                  <div key={i} className={`col-md-7 fade ${timer > 1 ? "show" : ""}`}>
+                    <div className="category-wrapper category-wrapper1">
+                      {item?.body && parse(item?.body)}
+                    </div>
+                  </div>
+                  <div key={i} className={`col-md-12 fade ${timer > 4 ? "show" : ""}`}>
+                    <div className="category-wrapper category-wrapper1 mt-15">
+                      {item?.images?.length ? <h2 className="text-center">{t("Galareya")}</h2> : ""}
+                      <div className="category-wrapper category-wrapper1 mt-15 row gy-4 p-0 mx-auto">
+                        {item?.images.map((el: { image: string }, j: number) => (
+                          <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12" key={j}>
+                            <div className="text-center brand-img-div" style={{ border: '1px solid #c3cccc' }}>
+                              <Image
+                                className="d-inline-block brand-img"
+                                src={el.image}
+                                alt="brand-img"
+                                width={200}
+                                height={300}
+                                style={{ width: 'auto', height: 'auto' }}
+                              />
+                            </div>
+                          </div>
+                        ))}
                       </div>
                     </div>
-                  ))}
-                </div>
-              </div>
-            </div>
+                  </div>
+                </>
+              ))
+            }
           </div>
         </div>
       </div>
--- src/components/shop/ShopSidebar.tsx
+++ src/components/shop/ShopSidebar.tsx
@@ -2,24 +2,30 @@
 
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { productData } from "@/data/product-data";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
 import api from "@/utils/api";
 import useQueryParam from "@/utils/useQueryParams";
 import { useParams } from "next/navigation";
+import parse from 'html-react-parser'
+
+
 const ShopSidebar = () => {
   const [sliderValues, setSliderValues] = useState([25]);
   const { getParams } = useQueryParam()
 
   const { t } = useTranslation()
   const [data, setData] = useState<any[]>([])
+  const [producs, setProducts] = useState<any[]>([])
+
   const params = useParams()
 
   const getData = async () => {
     const resp = await api.get(`common/categories/`)
+    const respProducts = await api.get(`common/recommended-products/`)
 
     setData(resp.data);
+    setProducts(respProducts.data)
   }
 
 
@@ -60,29 +66,31 @@ const ShopSidebar = () => {
                 {t("Ommabop mahsulotlar")}
               </h6>
               <div className="side-product mb-50">
-                {productData?.slice(6, 10)?.map((item, i) => (
+                {producs.map((item, i) => (
                   <div
                     key={i}
                     className="side-pro-wrapper d-flex align-items-start mb-15"
                   >
                     <div className="side-pro-img border-gray1 mr-10">
-                      <Link href={`/shop-details/${item?.id}`}>
+                      <Link href={`/shop-details/${item?.slug}`}>
                         <Image
-                          src={item?.img}
+                          src={item?.poster}
                           className="img-fluid"
                           alt="sidebar-img"
+                          width={200}
+                          height={200}
                         />
                       </Link>
                     </div>
                     <div className="side-pro-text">
                       <h6 className="pb-10">
-                        <Link href={`/shop-details/${item?.id}`}>
+                        <Link href={`/shop-details/${item?.slug}`}>
                           {" "}
                           {item?.title}{" "}
                         </Link>
                       </h6>
                       <span className="price font500">
-                        $17.50 <del>$19.50</del>
+                        {/* {parse(item?.description)} */}
                       </span>
                     </div>
                   </div>
