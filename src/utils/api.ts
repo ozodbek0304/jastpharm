c9541eb256b92d835930e453c76dfcd9eccdd94c
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-06-06 11:46:53 +0500

    baseurl

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'https://jaspharm.pythonanywhere.com/api/v1'
+const baseURL = 'https://admin-jaspharm.soffhub.uz/api/v1'
 
 const api = axios.create({
     baseURL
